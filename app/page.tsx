1f8c860fec436a25f406e9eee263ba70e7f947f3
Prajna1999 <gituprajna20@gmail.com>
2025-07-19 20:46:32 +0530

    feat: add India svg card

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
 import styles from "./page.module.css";
-import { Button } from "@/components/ui/button"
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
         <p>Hello world</p>
-        <Button >Click Me </Button>
       </main>
 
     </div >
