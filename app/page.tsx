3ec1466b4a4f890a6d587ba039f22258697c62cb
Prajna1999 <gituprajna20@gmail.com>
2025-07-19 17:26:36 +0530

    chore: setup tailwind and shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
-import Image from "next/image";
 import styles from "./page.module.css";
-
+import { Button } from "@/components/ui/button"
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
         <p>Hello world</p>
+        <Button >Click Me </Button>
       </main>
 
     </div >
